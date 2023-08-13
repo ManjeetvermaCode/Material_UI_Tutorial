3990f2c682fd4039d1492d6c0a34a7053750f3d1
manjeetverma <manjeetverma@gmail.com>
2023-08-13 21:46:44 +0530

    added event handler for everychange (onChange) and for form submit(onSubmit), to handle state changes.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
-import Typography from "./components/Typography"
-import Button  from './components/Buttons'
+
 import TextFields from "./components/textFields"
 // import './App.css'
 
@@ -7,11 +6,7 @@ function App() {
 
   return (
     <>
-     <h1>Typography Examples</h1>
-     <Typography/>
-     <h1>Button Examples</h1>
-     <Button/>
-     <h1>TextField Examples</h1>
+   
      <TextFields/>
     </>
   )
